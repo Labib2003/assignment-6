356585fffed3a2e7cca060a1f6438aeef579d2ca
Labib2003 <labib.salimi30@gmail.com>
2022-03-01 15:51:04 +0600

    sensor list revised

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-const searchField = document.getElementById('search-field')
+const searchField = document.getElementById('search-field');
 const searchBtn = document.getElementById('search-btn');
 const resultCardContainer = document.getElementById('result-card-container');
 const phoneDetails = document.getElementById('phone-details');
 
+// loading search results from api
 const loadData = (phoneName) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${phoneName}`;
     fetch(url)
@@ -10,12 +11,15 @@ const loadData = (phoneName) => {
         .then(data => displayResults(data));
 };
 
+// showing search results in ui
 const displayResults = (data) => {
+    // No result msg
     if (data.status === false) {
         const noResult = document.createElement('h3');
         noResult.innerText = 'No Result Found';
         resultCardContainer.appendChild(noResult);
     }
+    // showing search results
     else {
         const phones = data.data;
         phones.forEach(phone => {
@@ -29,7 +33,7 @@ const displayResults = (data) => {
                     <div class="card-body">
                         <h5 class="card-title">${phone.phone_name}</h5>
                         <p class="card-text">Brand: ${phone.brand}</p>
-                        <img src="${phone.image}" class="card-img-top">
+                        <img src="${phone.image}" class="card-img-top mb-5">
                         <a onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">More Info</a>
                     </div>
                 </div>
@@ -40,14 +44,15 @@ const displayResults = (data) => {
     };
 };
 
+// loading more info abaut a specific phone
 const loadPhoneDetails = (slug) => {
     const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPhoneDetails(data))
-        .then(data => console.log(data));
+        .then(data => displayPhoneDetails(data));
 };
 
+// displaying more info
 const displayPhoneDetails = (data) => {
     phoneDetails.innerHTML = '';
     phoneDetails.innerHTML = `
@@ -65,19 +70,20 @@ const displayPhoneDetails = (data) => {
                 <p>Memory: ${data.data.mainFeatures.memory}</p>
                 <p>Storage: ${data.data.mainFeatures.storage}</p>
             </div>
-            <h6>Sensors</h6>
             <div id="sensor-list"></div>
         </div>
     `;
     const sensorList = document.getElementById('sensor-list');
     const sensors = data.data.mainFeatures.sensors;
+    const h6 = document.createElement('h6');
+    h6.innerText = 'Sensors:';
     sensors.forEach(sensor => {
-        const p = document.createElement('p');
-        p.innerText = sensor;
-        sensorList.appendChild(p);
+        h6.innerText = h6.innerText + ',' + sensor;
+        sensorList.appendChild(h6);
     });
 };
 
+// button click handeler
 searchBtn.addEventListener('click', () => {
     resultCardContainer.innerHTML = '';
     phoneDetails.innerHTML = '';
