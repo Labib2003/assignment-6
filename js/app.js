8b75ba10133b67110a591333b4fc57c3ccb56c50
Labib2003 <labib.salimi30@gmail.com>
2022-03-01 14:01:53 +0600

    search results shown in the page

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 const searchField = document.getElementById('search-field')
 const searchBtn = document.getElementById('search-btn');
+const resultCardContainer = document.getElementById('result-card-container');
 
 const loadData = (phoneName) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${phoneName}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayResults(data));
+}
+
+const displayResults = (data) => {
+    const phones = data.data;
+    phones.forEach(phone => {
+        console.log(phone);
+        const resultCard = document.createElement('div');
+        resultCard.classList.add('col-sm-6');
+        resultCard.innerHTML = `
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">Brand: ${phone.brand}</p>
+                    <img src="${phone.image}" class="card-img-top">
+                    <a href="#" class="btn btn-primary">More Info</a>
+                </div>
+            </div>
+        `;
+        resultCardContainer.appendChild(resultCard);
+    });
 }
 
 searchBtn.addEventListener('click', () => {
